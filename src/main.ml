4dfbe9ba9b2b869a85f353b000257a864a1466ec
François Thiré <franth2@gmail.com>
2017-12-04 19:58:00 +0100

    A first attempt to add an LF encoding (DO NOT WORK)
    Need to factorize the code a little bit.

--- src/main.ml
+++ src/main.ml
@@ -1,20 +1,35 @@
+(* TODO:
+   - split the project in several modules
+   - Make an API so that it can be use as a library
+   - Use cmdliner
+*)
+
+type entry =
+  | Declaration of Basic.loc * Basic.ident * Signature.staticity * Term.term
+  | Definition of Basic.loc * Basic.ident * Term.term option * Term.term
+  | Opaque of Basic.loc * Basic.ident * Term.term option * Term.term
+  | Cmd of Cmd.command
+
 module type CONFIG =
 sig
-val set_meta_file : string -> unit
-val meta_file : unit -> string option
 
-val add_module_file : string -> unit
-val module_files : unit -> string list
+  val set_meta_file : string -> unit
+  val meta_file : unit -> string option
 
-val switch_beta_off : unit -> unit
-val beta : unit -> bool
+  val add_module_file : string -> unit
+  val module_files : unit -> string list
 
-val use_non_linear : unit -> unit
+  val switch_beta_off : unit -> unit
+  val beta : unit -> bool
 
-val set_output_file : string -> unit
-val output_file : unit -> string option
+  val use_non_linear : unit -> unit
 
-val print_version : unit -> unit
+  val set_output_file : string -> unit
+  val output_file : unit -> string option
+
+  val print_version : unit -> unit
+
+  val set_encoding : string -> unit
 end
 
 module Config:CONFIG =
@@ -24,6 +39,8 @@ struct
   let _module_files = ref []
   let _beta = ref true
   let _output_file = ref None
+  let _encoding = ref None
+
 
   let set_meta_file s =
     _meta_file := Some s
@@ -51,17 +68,145 @@ struct
 
   let print_version =
     Version.print_version
+
+  let set_encoding s =
+    _encoding := Some s
 end
 
+module type ENCODING =
+sig
+  open Basic
+  open Term
+
+  type t = entry
+  type ctx = loc * ident * term
+  type db = int
+
+  val md : mident
+
+  val signature : Signature.t
+
+  val encode_entry : entry -> t
+
+  val decode_entry : t -> entry
+
+end
+
+module LF:ENCODING =
+struct
+
+  open Basic
+  open Term
+
+  type t = entry
+  type ctx = loc * ident * term
+  type db = int
+
+  let md = mk_mident "LF"
+
+  let name_of str = mk_name md (mk_ident str)
+
+  let const_of str = mk_Const dloc (name_of str)
+
+  let signature =
+    let sg = Signature.make md in Signature.import sg dloc md; sg
+
+  let rec encode_term t =
+    match t with
+    | Kind -> assert false
+    | Type(lc) -> encode_type lc
+    | DB(lc,x,n) -> encode_DB lc x n
+    | Const(lc, name) -> encode_Const lc name
+    | Lam(lc,x,mty,te) -> encode_Lam lc x mty te
+    | App(f,a,args) -> encode_App f a args
+    | Pi(lc,x,a,b) -> encode_Pi lc x a b
+
+  and encode_type lc =
+    const_of "ty"
+
+  and encode_DB lc x n = mk_DB lc x n
+
+  and encode_Const lc name = mk_Const lc name
+
+  and encode_Lam lc x mty te = mk_Lam lc x mty te
+
+  and encode_App f a args = mk_App f a args
+
+  and encode_Pi lc x a b =
+    let a' = encode_term a in
+    mk_App (const_of "prod") a' [mk_Lam dloc x (Some a') (encode_term b)]
+
+  let encode_entry e =
+    match e with
+    | Declaration(lc,id,st,te) ->
+      Declaration(lc,id, st, encode_term te)
+    | Definition(lc,id,mty,te) ->
+      let ty = match mty with None -> Typing.inference signature te | Some ty -> ty in
+      let mty' = mk_App (const_of "eta") (encode_term ty) [] in
+      Definition(lc,id, Some mty',encode_term te)
+    | Opaque(lc,id,mty,te) ->
+      let ty = match mty with None -> Typing.inference signature te | Some ty -> ty in
+      let mty' = mk_App (const_of "eta") (encode_term ty) [] in
+      Opaque(lc,id, Some mty',encode_term te)
+    | Cmd(cmd) -> failwith "commands are not handled right now"
+
+  let rec decode_term t =
+    match t with
+    | Kind -> assert false
+    | Type(lc) -> assert false
+    | DB(lc,x,n) -> decode_DB lc x n
+    | Const(lc,name) -> decode_Const lc name
+    | Lam(lc,x,mty,te) -> decode_Lam lc x mty te
+    | App(f,a,args) -> decode_App f a args
+    | Pi(lc,x,a,b) -> decode_Pi lc x a b
+
+  and decode_DB lc x n = mk_DB lc x n
+
+  and decode_Const lc name =
+    if name_eq name (name_of "ty") then mk_Type dloc else mk_Const lc name
+
+  and decode_Lam lc x mty te =
+    let mty' = match mty with None -> None | Some mty -> Some (decode_term mty) in
+    mk_Lam lc x mty' (decode_term te)
+
+  and decode_App f a args =
+    match f with
+    | Const(lc,name) ->
+      if name_eq name (name_of "eta") then
+        match args with
+        | [] -> decode_term a
+        | x::args -> mk_App a x args
+      else if name_eq name (name_of "prod") then
+        match args with
+        | [Lam(_,x,Some a, b)] -> mk_Pi dloc x (decode_term a) (decode_term b)
+        | _ -> assert false
+      else
+        mk_App (decode_term f) (decode_term a) (List.map decode_term args)
+
+    | _ -> decode_App (decode_term f) (decode_term a) (List.map decode_term args)
+
+  and decode_Pi lc x a b = assert false
+
+  let rec decode_entry e =
+    match e with
+    | Declaration(lc,id,st,te) ->
+      Declaration(lc,id, st, decode_term te)
+    | Definition(lc,id,mty,te) ->
+      let mty' = match mty with None -> None | Some ty -> Some (decode_term ty) in
+      Definition(lc,id,mty', decode_term te)
+    | Opaque(lc,id,mty,te) ->
+      let mty' = match mty with None -> None | Some ty -> Some (decode_term ty) in
+      Opaque(lc,id,mty', decode_term te)
+    | Cmd(cmd) -> failwith "commands are not handled right now"
+end
 
 module Meta =
 struct
   open Config
   open Basic
 
-  let normalize term =
+  let normalize_meta meta_file term =
     let filter name =
-      let meta_file = mk_mident (match meta_file () with None -> "" | Some s -> s) in
       match name with
       | Rule.Delta(cst) -> List.mem (md cst) (List.map mk_mident @@ module_files ())
       | Rule.Gamma(_,cst) -> mident_eq (md cst) meta_file
@@ -75,6 +220,15 @@ struct
     match Env.reduction ~red:red Reduction.Snf term with
     | OK t -> t
     | Err err -> failwith "normalization error"
+
+  let normalize_encoding term =
+    failwith "todo"
+
+  let normalize term =
+    match meta_file () with
+    | None -> normalize_encoding term
+    | Some mf -> normalize_meta (mk_mident mf) term
+
 
   let mk_prelude lc id =
     Env.init id;
@@ -144,6 +298,7 @@ module P = Parser.Make(Meta)
 let args =
   let open Config in
   ["--meta-file", Arg.String set_meta_file, "The file containing the meta rules. It has to be typed checked";
+   "--encode", Arg.String set_encoding, "Encoding the Dedukti file. Only LF encoding is supported right now";
    "--switch-beta-off", Arg.Unit switch_beta_off, "switch off beta while normalizing terms";
    "--module", Arg.String add_module_file, "Normalize against constants defined in an other module";
    "--non-linear", Arg.Unit use_non_linear, "Allows to have non-linear meta-rules";
